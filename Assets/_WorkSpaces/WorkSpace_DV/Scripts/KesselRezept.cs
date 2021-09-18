65fedcd6f0b7cf265a39614e4d06b7879cb1884b
DavidVoe125 <david.voellmar@gmail.com>
2021-09-19 00:09:02 +0200

    .

--- Assets/_WorkSpaces/WorkSpace_DV/Scripts/KesselRezept.cs
+++ Assets/_WorkSpaces/WorkSpace_DV/Scripts/KesselRezept.cs
@@ -4,82 +4,89 @@ using UnityEngine;
 
 public class KesselRezept : MonoBehaviour
 {
-    public SpriteRenderer renderer;
+    public SpriteRenderer spriteRenderer;
     public Sprite[] sprites;
 
     // Start is called before the first frame update
     void Start()
     {
-        renderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 
         if (PlayerPrefs.GetInt("KesselItems") == 1)
         {
-            renderer.sprite = sprites[1];
+            spriteRenderer.sprite = sprites[1];
 
             PlayerPrefs.SetInt("KesselItems", 1);
         }
         else if (PlayerPrefs.GetInt("KesselItems") == 2)
         {
-            renderer.sprite = sprites[2];
+            spriteRenderer.sprite = sprites[2];
 
             PlayerPrefs.SetInt("KesselItems", 2);
         }
         else if (PlayerPrefs.GetInt("KesselItems") == 3)
         {
-            renderer.sprite = sprites[3];
+            spriteRenderer.sprite = sprites[3];
 
             PlayerPrefs.SetInt("KesselItems", 3);
         }
         else if (PlayerPrefs.GetInt("KesselItems") == 5)
         {
-            renderer.sprite = sprites[4];
+            spriteRenderer.sprite = sprites[4];
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        void OnTriggerStay2D(Collider2D col)
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.name == "Baldrian" && PlayerPrefs.GetInt("isBeingHeld") == 0 &&
+            PlayerPrefs.GetInt("KesselItems") == 0)
         {
-            if (col.name == "Baldrian" && PlayerPrefs.GetInt("isBeingHeld") == 0 && PlayerPrefs.GetInt("KesselItems") == 0)
-            {
-                print("+ Baldrian");
+            print("+ Baldrian");
 
-                renderer.sprite = sprites[1];
+            spriteRenderer.sprite = sprites[1];
 
-                PlayerPrefs.SetInt("KesselItems", 1);
-            }
-            else if (col.name == "Stinkesocke" && PlayerPrefs.GetInt("isBeingHeld") == 0 && PlayerPrefs.GetInt("KesselItems") == 1)
-            {
-                print("+ Stinkesocke");
-
-                renderer.sprite = sprites[2];
-
-                PlayerPrefs.SetInt("KesselItems", 2);
-            }
-            else if (col.name == "Kaugummi" && PlayerPrefs.GetInt("isBeingHeld") == 0 && PlayerPrefs.GetInt("KesselItems") == 2)
-            {
-                print("+ Kaugummi");
-
-                renderer.sprite = sprites[3];
-
-                PlayerPrefs.SetInt("KesselItems", 3);
-            }
-            else if (col.name == "Ehering" && PlayerPrefs.GetInt("isBeingHeld") == 0 && PlayerPrefs.GetInt("KesselItems") == 3)
-            {
-                print("+ Ehering");
-
-                renderer.sprite = sprites[4];
-
-                PlayerPrefs.SetInt("KesselItems", 4);
-            }
-            else if (PlayerPrefs.GetInt("KesselItems") == 5)
-            {
-                print("Hier gibt es nichts mehr zu tun.");
-            }
-            else
-            {
-                print("Das gehört hier nicht hin.");
-            }
+            PlayerPrefs.SetInt("KesselItems", 1);
         }
+        else if (col.name == "Stinkesocke" && PlayerPrefs.GetInt("isBeingHeld") == 0 &&
+                 PlayerPrefs.GetInt("KesselItems") == 1)
+        {
+            print("+ Stinkesocke");
+
+            spriteRenderer.sprite = sprites[2];
+
+            PlayerPrefs.SetInt("KesselItems", 2);
+        }
+        else if (col.name == "Kaugummi" && PlayerPrefs.GetInt("isBeingHeld") == 0 &&
+                 PlayerPrefs.GetInt("KesselItems") == 2)
+        {
+            print("+ Kaugummi");
+
+            spriteRenderer.sprite = sprites[3];
+
+            PlayerPrefs.SetInt("KesselItems", 3);
+        }
+        else if (col.name == "Ehering" && PlayerPrefs.GetInt("isBeingHeld") == 0 &&
+                 PlayerPrefs.GetInt("KesselItems") == 3)
+        {
+            print("+ Ehering");
+
+            spriteRenderer.sprite = sprites[4];
+
+            PlayerPrefs.SetInt("KesselItems", 4);
+        }
+        else if (PlayerPrefs.GetInt("KesselItems") == 5)
+        {
+            print("Hier gibt es nichts mehr zu tun.");
+        }
+        else
+        {
+            print("Das gehört hier nicht hin.");
+        }
+    }
 }
