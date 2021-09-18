96d873be26d15920faf96c302c197c1f5bfb2cc4
DavidVoe125 <david.voellmar@gmail.com>
2021-09-18 02:22:02 +0200

    Räume

--- Assets/Scripts/Scripts/DoorLevelTrigger.cs
+++ Assets/Scripts/Scripts/DoorLevelTrigger.cs
@@ -48,6 +48,8 @@ public class DoorLevelTrigger : MonoBehaviour
                 print("loadnextlvl");
 
                 SceneManager.LoadScene(nextLevel);
+
+                print(nextLevel);
                 }
             
             else
